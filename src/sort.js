84c9502ace8c0bd8982cf01c83661edfa993e79d
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-11-15 16:59:03 +0000

    test passing

--- src/sort.js
+++ src/sort.js
@@ -4,7 +4,19 @@ class Sort {
   constructor(){
 
   }
-  bubbleSort(){
+  bubbleSort(rack){
+
+   var len = rack.length;
+   for (var i = len-1; i>=0; i--){
+     for(var j = 1; j<=i; j++){
+       if(rack[j-1]>rack[j]){
+           var temp = rack[j-1];
+           rack[j-1] = rack[j];
+           rack[j] = temp;
+        }
+     }
+   }
+   return rack;
 
   }
 }
